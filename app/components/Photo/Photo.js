9f73631faf2cbae3bc95ca0a4f5902716068d9cb
Ryan Keller <RyanKeller@my.UNT.edu>
2016-12-05 19:31:54 -0800

    More skeleton.

--- app/components/Photo/Photo.js
+++ app/components/Photo/Photo.js
@@ -1,15 +1,9 @@
 // jshint ignore: start
 import React from 'react'
 
-class Photo extends React.Component {
-  render () {
-    return (
-      <div>
-        <h2>And this is our Photo!</h2>
-      </div>
-    )
-  }
-
-}
+const Photo = ({ image, title }) =>
+  <div>
+    <h2>And this is our Photo!</h2>
+  </div>
 
 export default Photo
--- app/components/PhotoViewer/PhotoViewer.js
+++ app/components/PhotoViewer/PhotoViewer.js
@@ -4,11 +4,21 @@ import React from 'react'
 import Photo from '../Photo/Photo'
 
 class PhotoViewer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      image: '',
+      title: ''
+    }
+  }
+
   render () {
     return (
       <div>
         <h1>This is our Photo Viewer</h1>
-        <Photo />
+        <Photo title={this.state.title}
+          image={this.state.image}
+        />
       </div>
     )
   }
