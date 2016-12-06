3e75ac18c92148c3740cf32b07ca3936b6bc9497
Ryan Keller <RyanKeller@my.UNT.edu>
2016-12-05 21:04:44 -0800

    Added changes to button, beginning to mutuate API call.

--- app/components/PhotoViewer/PhotoViewer.js
+++ app/components/PhotoViewer/PhotoViewer.js
@@ -8,7 +8,7 @@ import Photo from '../Photo/Photo'
 
 const placeholder = 'https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg'
 
-const key = '?api_key=' + 'bogXwr5jeRLfVzR3tAPxOE0ydqNqQISG9WIYTyvj'
+const key = 'bogXwr5jeRLfVzR3tAPxOE0ydqNqQISG9WIYTyvj'
 const endpoint = 'https://api.nasa.gov/planetary/apod'
 
 class PhotoViewer extends React.Component {
@@ -16,16 +16,26 @@ class PhotoViewer extends React.Component {
     super(props)
     this.state = {
       photo: {
-        title: 'Title Here',
-        date: 'Date',
-        photoURL: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg'
+        title: '',
+        date: '',
+        photoURL: ''
       }
     }
   }
 
   getPhoto () {
-    let url = endpoint + key
-    console.log('URL', url)
+    
+    let url = ''
+    if (this.state.photo.date) {
+      url = endpoint + '?api_key=' + key
+      /*
+      url = endpoint + '?date=' + '&api_key=' + key
+      console.log('URL', url)
+      //https://api.nasa.gov/planetary/apod?date=2016-12-04&api_key=PYWcKr6z1DFCKtPoEtXL8kpK5T2CeLca5z8B7dum
+      */
+    } else {
+      url = endpoint + '?api_key=' + key
+    }
 
     axios.get(url)
     .then((res) => {
@@ -48,10 +58,11 @@ class PhotoViewer extends React.Component {
   
 
   render () {
+    let buttonText = (this.state.photo.date === '' ? "Get today's photo" : "Get yesterday's photo")
     return (
       <div>
         <h1>This is our Photo Viewer</h1>
-        <button onClick={this.getPhoto.bind(this)}>Get some photos!</button>
+        <button onClick={this.getPhoto.bind(this)}>{buttonText}</button>
         <Photo
           title={this.state.photo.title}
           date={this.state.photo.date}
